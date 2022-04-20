fab6e3b024cef0a402e1e9fba35b14c59d5d2e95
Luigi Zuniga <luigialbertozuniga@gmail.com>
2022-04-19 23:19:17 -0400

    webpack alias y referencias a imagenges se agrega menu en header

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,6 +9,9 @@ import RecoveryPassword from '../containers/RecoveryPassword';
 //Pages
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
 
 //Styles
 import '../styles/global.css';
@@ -21,7 +24,23 @@ const App =()=> {
                 <Route path="/" element={ <Home/> } />
                 <Route path="/login" element={ <Login/> }/>
                 <Route path="/recovery-password" element={ <RecoveryPassword/> }/>
+
+                <Route path="/send-email" element={ <SendEmail/> }/>
+                <Route path="/new-password" element={ <NewPassword/> }/>
+                <Route path="/account" element={ <MyAccount/> }/>
+
+
+                {/*
+
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+                */}
+
+
                 <Route path="*" element={ <NotFound/> } />
+
             </Routes>
         </Layout>
     </BrowserRouter>
